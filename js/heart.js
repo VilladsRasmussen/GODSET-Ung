a3a6f0dd254f654ed4f8d456d41b808f47c71834
FearTheV <villadsrasmussen1@gmail.com>
2025-01-08 11:52:54 +0100

    Tilføjet lille note ved tilmeldning af begivenhed

--- js/heart.js
+++ js/heart.js
@@ -1,6 +1,6 @@
 function myFunction() {
-  const element = document.getElementById("heart-react");  // Get the DIV element
-  element.classList.remove("fa-regular"); // Remove mystyle class from DIV
-  element.classList.add("fa-solid"); // Add newone class to DIV
-  element.classList.add("hjerte-fuld"); // Add newone class to DIV
+  const element = document.getElementById("heart-react");
+  element.classList.remove("fa-regular"); 
+  element.classList.add("fa-solid"); 
+  element.classList.add("hjerte-fuld"); 
 }
