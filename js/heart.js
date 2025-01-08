afadb9ee64a81e37ce08c7efd3e4f632fc4965f6
FearTheV <villadsrasmussen1@gmail.com>
2025-01-08 09:45:39 +0100

    Ændret dato på forside

--- js/heart.js
+++ js/heart.js
@@ -2,4 +2,5 @@ function myFunction() {
   const element = document.getElementById("heart-react");  // Get the DIV element
   element.classList.remove("fa-regular"); // Remove mystyle class from DIV
   element.classList.add("fa-solid"); // Add newone class to DIV
+  element.classList.add("hjerte-fuld"); // Add newone class to DIV
 }
